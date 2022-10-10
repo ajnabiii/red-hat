575cfd97a3559e41b88d65522af8b50e76f3463a
Taher Hussain <85551107+ajnabiii@users.noreply.github.com>
2022-10-10 17:01:27 +0530

    Add files via upload

---
+++ anagram.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class anagram{
+    static Scanner sc = new Scanner(System.in);
+    public static boolean check_anagram(String str1, String str2){
+        // Finding the lengths of two strings
+        int str1_length = str1.length();
+        int str2_length = str2.length();
+
+        // Checking the base condition that if two strings have different lengths then it can't form anagaram
+        if(str1_length != str2_length){
+            return false;
+        }
+
+        // Storing str1 characters in char_array1 and str2 characters in char_array2
+        char[] char_array1 = str1.toCharArray();
+        char[] char_array2 = str2.toCharArray();
+
+        // sort the char array of str1 and str2
+        Arrays.sort(char_array1);
+        Arrays.sort(char_array2);
+
+        // Returning the result
+        return Arrays.equals(char_array1, char_array2);
+    }
+    public static void main(String args[]){
+        
+        //Enter the strings below
+
+        System.out.print("Enter first string : ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string : ");
+        String str2 = sc.nextLine();
+
+        //Result
+
+        System.out.println(check_anagram(str1, str2));
+
+    }
+}
